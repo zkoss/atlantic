97273b301196c837b9fe5ca22afac0cebe60f381
hanhsu <hanhsu@potix.com>
2014-12-09 09:09:43 +0000

    Release 1.0.4

--- src/main/java/org/zkoss/theme/atlantic/Version.java
+++ src/main/java/org/zkoss/theme/atlantic/Version.java
@@ -3,5 +3,5 @@ package org.zkoss.theme.atlantic;
 public class Version {
 	/** Returns the version UID.
 	 */
-	public static final String UID = "1.0.3";
+	public static final String UID = "1.0.4";
 }

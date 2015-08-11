7893d6dac294e2abbefac83059d30134af1ff5d4
DevChu <marchuqq@gmail.com>
2015-08-11 14:17:02 +0800

    upgrade to version 1.0.7

--- src/main/java/org/zkoss/theme/atlantic/Version.java
+++ src/main/java/org/zkoss/theme/atlantic/Version.java
@@ -3,5 +3,5 @@ package org.zkoss.theme.atlantic;
 public class Version {
 	/** Returns the version UID.
 	 */
-	public static final String UID = "1.0.6";
+	public static final String UID = "1.0.7";
 }
